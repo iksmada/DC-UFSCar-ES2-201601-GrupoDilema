1dd219a10a8ffe9504c2e3ba683a387848cb358d
Raphael Adamski <raphaeladamski@hotmail.com>
2016-05-20 17:09:48 -0300

    melhorando parser CSV

--- src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
+++ src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
@@ -3,7 +3,7 @@ package net.sf.jabref.importer.fileformat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -35,6 +35,49 @@ public class CSVImporter extends ImportFormat {
     @Override
     public String getCLIId() {
         return "csv";
+    }
+
+    @Override
+    public boolean isRecognizedFormat(InputStream stream) throws IOException {
+
+        BufferedReader in = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(stream));
+
+        String str;
+
+        while ((str = in.readLine()) != null) {
+            if (CSVImporter.CSV_PATTERN.matcher(str).find()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<BibEntry> importEntries(InputStream stream, OutputPrinter status) throws IOException {
+        List<BibEntry> bibitems = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(stream))) {
+            String str;
+            while ((str = in.readLine()) != null) {
+                sb.append(str);
+                sb.append('\n');
+            }
+        }
+        //divide a entrada em 2 Strings, a primeira é o cabeçalho indicando os campos, o resto são as entradas
+        String[] entries = sb.toString().split("\n", 2);
+
+        //divide o cabeçalho nas virgulas
+        String[] fields = entries[0].split(",");
+
+        //fields[0]=tipo da entrada-- unica exigencia
+
+        for (String entry : entries) {
+            BibEntry b = new BibEntry(DEFAULT_BIBTEXENTRY_ID, fields[0]);
+
+        }
+
+        return bibitems;
     }
 
 }
